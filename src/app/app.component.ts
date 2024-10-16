998ef058c70afc120970f870d72a92b8decdae97
mmullerx4 <mmullerx4@gmail.com>
2024-10-16 19:17:52 -0400

    w05-tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { UserService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
 export class AppComponent {
-  title = 'services-assignment-start';
+
+
 }
